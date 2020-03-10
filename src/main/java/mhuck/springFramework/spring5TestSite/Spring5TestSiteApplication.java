546a6467fca3bdd8f72ad2e41ffa849e3d762d05
distilled4x <Matt Huck>
2020-03-09 18:53:25 -0700

    First commit

---
+++ src/main/java/mhuck/springFramework/spring5TestSite/Spring5TestSiteApplication.java
@@ -0,0 +1,13 @@
+package mhuck.springFramework.spring5TestSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5TestSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5TestSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mhuck/springFramework/spring5TestSite/Spring5TestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mhuck.springFramework.spring5TestSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5TestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
